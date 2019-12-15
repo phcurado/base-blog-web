3c0df92b749c6c34d9d725a4a3e91d6dc8641450
Paulo Henrique de Oliveira Curado <phcurado@gmail.com>
2019-12-15 10:56:12 -0300

    adicionado editor de texto no blo

--- src/pages/postPage/postEditPage.js
+++ src/pages/postPage/postEditPage.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import PostService from '../../services/postService';
-import { FormControl, InputLabel, Input, Grid, Button, Container } from '@material-ui/core';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  Grid,
+  Button,
+  Container,
+  TextField,
+  FormLabel
+} from '@material-ui/core';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const PostEditPage = props => {
   const [title, setTitle] = useState([]);
   const [abstract, setAbstract] = useState([]);
+  const [textData, setTextData] = useState([]);
 
   let { id } = useParams();
 
@@ -13,10 +25,11 @@ const PostEditPage = props => {
     const { data } = await PostService.getById(id);
     setTitle(data.title);
     setAbstract(data.abstract);
+    setTextData(data.html);
   };
 
   const addPost = async () => {
-    let post = { title, abstract, status: 1 };
+    let post = { title, abstract, status: 1, html: textData };
     if (id) {
       return PostService.put(id, post).then(() => {
         props.history.push('/posts');
@@ -39,13 +52,25 @@ const PostEditPage = props => {
       <Grid container direction="column" spacing={4}>
         <Grid container direction="row" item>
           <Grid container direction="column" item>
-            <FormControl>
+            <FormControl margin="dense">
               <InputLabel htmlFor="title">Título</InputLabel>
               <Input id="title" value={title} onChange={e => setTitle(e.target.value)} />
             </FormControl>
-            <FormControl>
+            <FormControl margin="dense">
               <InputLabel htmlFor="abstract">Resumo</InputLabel>
               <Input id="abstract" value={abstract} onChange={e => setAbstract(e.target.value)} />
+            </FormControl>
+            <FormControl margin="dense">
+              <FormLabel>Texto</FormLabel>
+              <CKEditor
+                editor={ClassicEditor}
+                data={textData ? textData : 'Digite seu texto aqui'}
+                onInit={editor => {
+                  // You can store the "editor" and use when it is needed.
+                  console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => setTextData(editor.getData())}
+              />
             </FormControl>
           </Grid>
         </Grid>
